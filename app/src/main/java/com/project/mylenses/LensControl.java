29ca7c1c94bff02f9f71f1c6500fa3224dd39bb7
Yuri Kireev <yura.kireev.vt@gmail.com>
2022-02-19 11:46:09 +0300

    Initial commit

---
+++ app/src/main/java/com/project/mylenses/LensControl.java
@@ -0,0 +1,52 @@
+package com.project.mylenses;
+
+import java.util.Date;
+
+public class LensControl {
+
+    private Integer nowCount;
+    private Integer endCount;
+    private Date nowDate;
+    private Date endDate;
+
+    /*
+    LensControl()
+    {
+        nowCount = 0;
+        endCount = 0;
+    }
+    public LensControl(Integer now, Integer end)
+    {
+        nowCount = now;
+        endCount = end;
+    }
+
+    public void setNowCount(int now)
+    {
+        nowCount = now;
+    }
+    public void setEndCount(int end)
+    {
+        endCount = end;
+    }
+
+    public int getNowCount()
+    {
+        return nowCount;
+    }
+    public int getEndCount()
+    {
+        return endCount;
+    }
+
+    public void nowCountPlus()
+    {
+        nowCount += 1;
+    }
+    public void nowCountMinus()
+    {
+        nowCount -= 1;
+    }
+    */
+
+}
---
+++ app/src/main/java/com/project/mylenses/MainActivity.java
@@ -0,0 +1,83 @@
+package com.project.mylenses;
+
+import static android.content.ContentValues.TAG;
+import static com.project.mylenses.R.id.TextLostUses;
+import static com.project.mylenses.R.id.toSettings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UpdateLostUses();
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == toSettings){
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    public void UpdateLostUses(){
+
+        TextView textLostUses = findViewById(TextLostUses);
+        textLostUses.setText("try");
+    }
+
+
+}
